c070af5092281aee2424ab90985edbbdf1630642
Averroes20 <andixray123@gmail.com>
2024-06-11 10:12:34 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './main-components/dashboard/dashboard.component';
+import { NavbarComponent } from './property/navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
